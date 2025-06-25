c9a4011b48d85d7f8bcf9d44816f4f7801b9503a
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2025-06-25 00:47:15 -0700

    Implemented | Second Largest Digit in a String

---
+++ SecondLargestDigitinaString/second_largest_digit_in_a_string.py
@@ -0,0 +1,13 @@
+```python
+def second_highest(s: str) -> int:
+    digits = set()
+    for char in s:
+        if char.isdigit():
+            digits.add(int(char))
+    
+    if len(digits) < 2:
+        return -1
+    
+    digits = sorted(list(digits), reverse=True)
+    return digits[1]
+```
