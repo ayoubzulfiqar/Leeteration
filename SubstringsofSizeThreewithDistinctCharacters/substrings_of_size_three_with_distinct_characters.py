c376ce375522a40dc11bdfda1f8a2f5a4c3d53ac
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2025-06-25 00:59:56 -0700

    Implemented | Substrings of Size Three with Distinct Characters

---
+++ SubstringsofSizeThreewithDistinctCharacters/substrings_of_size_three_with_distinct_characters.py
@@ -0,0 +1,9 @@
+```python
+def countGoodSubstrings(s):
+    count = 0
+    for i in range(len(s) - 2):
+        substring = s[i:i+3]
+        if len(set(substring)) == 3:
+            count += 1
+    return count
+```
