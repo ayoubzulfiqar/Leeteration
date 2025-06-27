e521c870c8e4f30675218da7fe8adeb81f6b716d
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2025-06-27 00:10:52 -0700

    Implemented | Find the Peaks

---
+++ FindthePeaks/find_the_peaks.py
@@ -0,0 +1,6 @@
+def findPeaks(mountain: list[int]) -> list[int]:
+    peaks = []
+    for i in range(1, len(mountain) - 1):
+        if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]:
+            peaks.append(i)
+    return peaks
