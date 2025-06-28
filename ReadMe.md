76323e4fe57519a901ed4fd4ce9ba73c432e9a3d
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2025-06-28 01:52:13 -0700

    Implemented | Valid Word

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,6 @@
 
 ## Easy
+* [Find the Integer Added to Array I](FindtheIntegerAddedtoArrayI/find_the_integer_added_to_array_i.py)
 * [Make a Square with the Same Color](MakeaSquarewiththeSameColor/make_a_square_with_the_same_color.py)
 * [Count the Number of Special Characters I](CounttheNumberofSpecialCharactersI/count_the_number_of_special_characters_i.py)
 * [Latest Time You Can Obtain After Replacing Characters](LatestTimeYouCanObtainAfterReplacingCharacters/latest_time_you_can_obtain_after_replacing_characters.py)
---
+++ ValidWord/valid_word.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+
+        has_vowel = False
+        has_consonant = False
+        vowels = "aeiouAEIOU"
+
+        for char in word:
+            if not char.isalnum():
+                return False
+
+            if char.isalpha():
+                if char in vowels:
+                    has_vowel = True
+                else:
+                    has_consonant = True
+        
+        return has_vowel and has_consonant
